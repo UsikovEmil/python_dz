22290827932a37593de739f12f64c9bb5a14f676
Emil <Usikov.emil@yandex.ru>
2023-05-25 10:52:41 +0500

    Задание 36 готово

--- DZ07/main_36.py
+++ DZ07/main_36.py
@@ -4,14 +4,21 @@
 # Нумерация строк и столбцов идет с единицы (подумайте, почему не с нуля). 
 # Примечание: бинарной операцией называется любая операция, у которой ровно два аргумента, как, например, у операции умножения.
 
-# *Пример:*
+#Пример:
 
-# **Ввод:** `print_operation_table(lambda x, y: x * y) ` 
-# **Вывод:**
-# 1 2 3 4 5 6
+#Ввод: `print_operation_table(lambda x, y: x * y) ` 
+#Вывод:     1       2       3       4       5       6
+#           2       4       6       8       10      12
+#           3       6       9       12      15      18
+#           4       8       12      16      20      24
+#           5       10      15      20      25      30
+#           6       12      18      24      30      36
 
-# 2 4 6 8 10 12
-# 3 6 9 12 15 18
-# 4 8 12 16 20 24
-# 5 10 15 20 25 30
-# 6 12 18 24 30 36
+
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    for i in range(1, num_rows + 1):
+        for j in range(1, num_columns + 1):
+            print(operation(i, j), end='\t')
+        print()
+
+print_operation_table(lambda x, y: x * y)
