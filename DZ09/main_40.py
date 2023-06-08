a050d432359b98e42014a112c6e21cdc20f379e9
Emil <Usikov.emil@yandex.ru>
2023-06-08 16:50:20 +0500

    Не работает код

--- DZ09/main_40.py
+++ DZ09/main_40.py
@@ -4,29 +4,18 @@
 
 
 # ____________________
-# import pandas as pd
-
-# # Загрузка файла и создание DataFrame
-
-# # df = pd.read_csv('sample_data/california_housing_train.csv')
-# df = open('E:\C#\Python\python_dz\DZ09\sample_data\california_housing_train.csv')
-
-# # Фильтрация строк по значению population
-# df_filtered = df[(df['population'] >= 0) & (df['population'] <= 500)]
-
-# # Вычисление средней стоимости дома
-# mean_house_value = df_filtered['median_house_value'].mean()
-
-# print(f'Средняя стоимость дома с количеством людей от 0 до 500 равна {mean_house_value:.2f} долларов.')
-# ----------------------
 import pandas as pd
+
+# Загрузка файла и создание DataFrame
 
 df = pd.read_csv('sample_data/california_housing_train.csv')
 
-filtered_df = df[(df['population'] >= 0) & (df['population'] <= 500)]
 
-print(filtered_df.head(10))
+# Фильтрация строк по значению population
+df_filtered = df[(df['population'] >= 0) & (df['population'] <= 500)]
 
-mean_house_valie = filtered_df['median_house_value'].mean()
+# Вычисление средней стоимости дома
+mean_house_value = df_filtered['median_house_value'].mean()
 
-print('Средняя стоимость дома, где количество людей от 0 до 500 --> ', mean_house_valie)
+print(f'Средняя стоимость дома с количеством людей от 0 до 500 равна {mean_house_value:.2f} долларов.')
+# ----------------------
